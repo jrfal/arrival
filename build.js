8d76c6f691217e166097e8f11c0a8239c826b5a3
Jeff Fal <jefffal@gmail.com>
2025-07-06 10:54:05 -0600

    A bunch of partials and a register page

--- build.js
+++ build.js
@@ -5,24 +5,40 @@ import handlebars from 'handlebars'
 
 const __dirname = path.resolve()
 
+const templatesDir = path.join(__dirname, 'templates')
 const templatePath = path.join(__dirname, 'templates', 'login.hbs')
 const dataDir = path.join(__dirname, 'data')
 const partialsDir = path.join(__dirname, 'partials')
 const outputDir = path.join(__dirname, 'dist')
 
+const loopOnDir = dir => fn => fs.readdirSync(dir).forEach(fn);
+
 // Register partials
-fs.readdirSync(partialsDir).forEach(file => {
+loopOnDir(partialsDir)(file => {
   const partial = fs.readFileSync(path.join(partialsDir, file), 'utf8')
   handlebars.registerPartial(path.parse(file).name, partial)
 })
 
-// Compile and render
-const templateSource = fs.readFileSync(templatePath, 'utf8')
-const template = handlebars.compile(templateSource)
+// Compile templates
+let templates = {};
+loopOnDir(templatesDir)(file => {
+  const templatePath = path.join(templatesDir, file)
+  const name = path.parse(file).name // e.g. 'index', 'about'
+
+  const templateSource = fs.readFileSync(templatePath, 'utf8')
+  templates[name] = handlebars.compile(templateSource)
+});
+
+// Compile partials
+loopOnDir(partialsDir)(file => {
+  const partialPath = path.join(partialsDir, file)
+  const name = path.parse(file).name // e.g. 'index', 'about'
+  handlebars.registerPartial(name, fs.readFileSync(partialPath, 'utf8'))
+})
 
 // Compile each data file
-fs.readdirSync(dataDir).forEach(dataFile => {
-  const name = path.parse(dataFile).name // e.g. 'index', 'about'
+loopOnDir(dataDir)(file => {
+  const name = path.parse(file).name // e.g. 'index', 'about'
   if (!name.match(/^\_/)) {
     const dataPath = path.join(dataDir, `${name}.json`)
     const globalPath = path.join(dataDir, `_global.json`)
@@ -35,7 +51,7 @@ fs.readdirSync(dataDir).forEach(dataFile => {
       data = { ...data, ...JSON.parse(fs.readFileSync(globalPath, 'utf8')) }
     }
   
-    const html = template(data)
+    const html = templates[data.template || "login"](data)
   
     const outputPath = path.join(outputDir, name === 'index' ? 'index.html' : `${name}.html`)
     fs.mkdirSync(outputDir, { recursive: true })
