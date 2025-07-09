497961b888ef63c774d153aa6aba47716b3d941e
Jeff Fal <jefffal@gmail.com>
2025-07-09 07:45:10 -0600

    Basic pico and pure css styles

--- build.js
+++ build.js
@@ -43,7 +43,8 @@ loopOnDir(dataDir)(file => {
     const dataPath = path.join(dataDir, `${name}.json`)
     const globalPath = path.join(dataDir, `_global.json`)
     const frameworkPath = path.join(dataDir, `_bootstrap.json`)
-    // const frameworkPath = path.join(dataDir, `_pico.json`)
+    //const frameworkPath = path.join(dataDir, `_pico.json`)
+    // const frameworkPath = path.join(dataDir, `_pure.json`)
   
     let data = {}
     if (fs.existsSync(dataPath)) {
